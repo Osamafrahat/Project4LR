dd2fdf74d54131e938966f2a2f4e255724b9a19b
MosaTech <osamaelmesady1@gmail.com>
2023-01-17 15:54:33 +0200

    Create a fake data source to act as a double to the real data source ,provide testing to the RemindersListViewModel and its live data objects ,provide testing to the SaveReminderView and its live data objects and fix some errors

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,28 +1,47 @@
-package com.udacity.project4.locationreminders.data
+package com.udacity.projepackage com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error(ERROR_MESSAGE)
+        }
+        return Result.Success(ArrayList(reminders))
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError) {
+            return Result.Error(ERROR_MESSAGE)
+        }
+        reminders?.let {
+            for (reminder in it) {
+                if (id == reminder.id) {
+                    return Result.Success(reminder)
+                }
+            }
+        }
+        return Result.Error("Reminder not found")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 
+    companion object {
+        const val ERROR_MESSAGE = "Error message"
+    }
 }
